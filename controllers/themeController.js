3b7eba3b1c6ded9bb9eed3161324b3a51449504f
v-aguiar <v.aguiar.dev@gmail.com>
2022-05-20 20:57:44 -0300

    Minor corrections

--- controllers/themeController.js
+++ controllers/themeController.js
@@ -1,16 +1,5 @@
 import db from "../db/db.js";
 
-export async function getTheme(req, res) {
-  const { name } = req.headers;
-
-  try {
-    const theme = await db.collection("temas").findOne({ name });
-    res.status(200).json(theme);
-  } catch {
-    res.status(500).send("Erro ao pegar o tema");
-  }
-}
-
 export async function getThemes(req, res) {
   try {
     const themes = await db.collection("temas").find();
