fa8d3854708054536c6de8c0e631284c798dd879
v-aguiar <v.aguiar.dev@gmail.com>
2022-05-20 16:56:50 -0300

    Initialize api project structure and deploy to Heroku

---
+++ db/db.js
@@ -0,0 +1,18 @@
+﻿import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const URL = process.env.MONGO_URL || "mongodb://localhost:27017";
+const mongoClient = new MongoClient(URL);
+
+let db = null;
+
+try {
+  await mongoClient.connect();
+  db = mongoClient.db("hackaton2-api");
+} catch (e) {
+  console.error("⚠ Could not connect to MongoDB!");
+}
+
+export default db;
