258301e20b8522f7c37ebacfaf1baf8e9c74275a
v-aguiar <v.aguiar.dev@gmail.com>
2022-05-20 21:26:14 -0300

    Configure subject route param

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -1,10 +1,14 @@
+import { ObjectId } from "mongodb";
 import db from "../db/db.js";
 
 export async function getMaterias(req, res) {
   const { id } = req.params;
 
   try {
-    const materias = await db.collection("materias").find({ id }).toArray();
+    const materias = await db
+      .collection("materias")
+      .find({ materiaId: new ObjectId(id) })
+      .toArray();
     res.status(200).json(materias);
   } catch {
     res.status(500).send("Erro ao pegar as materias");
--- routes/subjectRouter.js
+++ routes/subjectRouter.js
@@ -3,6 +3,6 @@ import { getMaterias } from "../controllers/subjectController.js";
 
 const subjectRouter = Router();
 
-subjectRouter.get("/materias", getMaterias);
+subjectRouter.get("/materias/:id", getMaterias);
 
 export default subjectRouter;
